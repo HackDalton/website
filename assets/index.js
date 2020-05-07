7e942afd5f326f46ef69e36393a63171791bedf2
William Barkoff <william@barkoffusa.com>
2020-05-07 14:09:35 -0400

    Make navbar sticky

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,19 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var burger = document.querySelector(".navbar-burger");
-    var navbarMenu = document.querySelector(".navbar-menu");
-    burger.addEventListener("click", function () {
-        burger.classList.toggle("is-active");
-        navbarMenu.classList.toggle("is-active");
-    });
+var burger = document.querySelector(".navbar-burger");
+var navbarMenu = document.querySelector(".navbar-menu");
+burger.addEventListener("click", function () {
+    burger.classList.toggle("is-active");
+    navbarMenu.classList.toggle("is-active");
 });
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function () {
+    if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+        document.querySelector(".navbar").classList.remove("is-transparent")
+        document.querySelector(".navbar").classList.add("is-info")
+        document.querySelector(".navbar").classList.add("is-fixed-top")
+    } else {
+        document.querySelector(".navbar").classList.remove("is-info")
+        document.querySelector(".navbar").classList.add("is-transparent")
+        document.querySelector(".navbar").classList.add("is-fixed-top")
+    }
+};
